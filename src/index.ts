e60fefe9a46a0dbb1777f122ee97e74544742c74
robonexx <robertwagar@gmail.com>
2023-12-06 10:55:29 +0100

    successfully connected to mongo db

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ const mongoose = require('mongoose');
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
+const MongoURI = process.env.MONGO_DB_URI || undefined;
+
+console.log(MongoURI)
 
 app.get("/", (req: Request, res: Response): void => {
   res.send("Hello Typescript with Node.js! Connected");
@@ -14,4 +17,9 @@ app.get("/", (req: Request, res: Response): void => {
 app.listen(PORT, (): void => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+mongoose.Promise = Promise
+
+mongoose.connect(MongoURI)
+mongoose.connection.on('error', (err: Error) => console.log(err))
 
